09a1b60507617fe03de22ee94ac1ff8ec7708e71
vibhutiReddoorz <vibhuti.kapoor@reddoorz.com>
2025-09-02 12:24:27 +0530

    Fan done, Tubelight done, Tv done

--- Appliance/Fan.rb
+++ Appliance/Fan.rb
@@ -2,16 +2,16 @@ require_relative 'Appliances'
 
 class Fan<Appliances
      
-    @speed=0
+    attr_reader :speed
     def initialize(speed)
         super('Fan')
       @speed=speed
     end
 
-    def change_speed(level)
-       return {success: false, message: "The fan is at its minimum speed!!"} if speed<0
-       return {success:false, message: "The fan is at its maximum speed!!"} if speed>5
+    def change_speed(speed)
+       return {success: false, message: "#{@speed} speed is too low!!"} if speed<0
+       return {success:false, message: "#{@speed} speed is too high!!"} if speed>5
        @speed=speed
-       return {success:true, message: "The speed is set to #{@speed}"}
+       return {success:true, message: "The speed is set to #{@speed}."}
     end
 end
