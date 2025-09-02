ecf79411ab6ca881f0e262000854af5ea81b3ebb
vibhutiReddoorz <vibhuti.kapoor@reddoorz.com>
2025-09-02 12:02:17 +0530

    Update Fan.rb

--- Appliance/Fan.rb
+++ Appliance/Fan.rb
@@ -3,7 +3,12 @@ require_relative 'Appliances'
 class Fan<Appliances
      
     @speed=0
-    def fan_speed
-     
+    def initialize(speed)
+        super('Fan')
+      @speed=speed
+    end
+
+    def change_speed(level)
+       return {}
     end
 end
