2e95ca4920038f6b2ab9d962e2b8ab7a1e099d9d
vibhutiReddoorz <vibhuti.kapoor@reddoorz.com>
2025-09-02 11:47:06 +0530

    Structure

---
+++ Appliance/Fan.rb
@@ -0,0 +1,9 @@
+require_relative 'Appliances'
+
+class Fan<Appliances
+     
+    @speed=0
+    def fan_speed
+     
+    end
+end
---
+++ Appliance/Tv.rb
@@ -0,0 +1,5 @@
+require_relative 'Appliances'
+
+class Tv<Appliances
+    
+end
