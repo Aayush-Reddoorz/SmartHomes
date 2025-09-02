9a3338bf1eee9abae1b7c0a0e8753f988bf24652
NaveenRedDoorz <naveen.chaudhary@reddoorz.com>
2025-09-02 13:50:02 +0530

    Appliance.rb common method

--- Appliance/Fan.rb
+++ Appliance/Fan.rb
@@ -3,8 +3,9 @@ require_relative 'Appliances'
 class Fan<Appliances
      
     attr_reader :speed
+
     def initialize
-    super('Fan')
+      super('Fan')
       @speed=5
     end
 
@@ -15,3 +16,6 @@ class Fan<Appliances
        return {success:true, message: "The speed is set to #{@speed}."}
     end
 end
+
+current_fan = Fan.new
+current_fan.display_edit_confurigation
